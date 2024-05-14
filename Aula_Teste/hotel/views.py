a64118b9b85e4353c4bed6cf5e8a41582586d58c
alemes7 <alemesferreira7@gmail.com>
2024-05-14 10:17:31 -0300

    first commit

---
+++ Aula_Teste/hotel/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse #render, renderiza a pagina html e HttpResponse, retorna uma resposta ao cliente
+from .models import Hotel #importa o model hotel
+
+# Create your views here.
+def homepage(request):
+    context = {}
+    dados_hotel = Hotel.objects.all()
+    context['dados_hotel'] = dados_hotel
+    return render(request, 'homepage.html', context) #retorna a pagina homepage.html
