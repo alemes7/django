76321e79049330aa3f08576da088e9d6ecfdbb38
alemes7 <alemesferreira7@gmail.com>
2024-05-14 16:22:00 -0300

    update reserve ja

--- Aula_Teste/hotel/views.py
+++ Aula_Teste/hotel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse #render, renderiza a pagina html e HttpResponse, retorna uma resposta ao cliente
 from .models import Hotel #importa o model hotel
+from .models import quarto #importa o model quarto
 
 # Create your views here.
 def homepage(request):
@@ -10,6 +11,6 @@ def homepage(request):
 
 def reserve(request):
     context = {}
-    dados_hotel = Hotel.objects.all()
-    context['dados_hotel'] = dados_hotel
-    return render(request, 'reserve.html') #retorna a pagina quartos.html
+    dados_quarto = quarto.objects.all()
+    context['dados_quarto'] = dados_quarto
+    return render(request, 'reserve.html', context) #retorna a pagina quartos.html
